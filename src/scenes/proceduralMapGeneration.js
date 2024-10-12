185c23c623e3b50d6d662bd59b25be9dcc6c5918
Justin-Lam <justinflam9225k@gmail.com>
2024-10-12 16:34:37 -0700

    Refactored code

--- src/scenes/proceduralMapGeneration.js
+++ src/scenes/proceduralMapGeneration.js
@@ -1,11 +1,20 @@
 class ProceduralMapGeneration extends Phaser.Scene
 {
-	// Parameters:
-	xOffset = 0;		// less = left, more = right
-	yOffset = 0;		// less = down, more = up
-	frequency = 0.105;	// less = less diversity/zoom in, more = more diversity/zoom out
-	numOctaves = 4;		// less = simpler, more = more complicated
-	
+	// Texture Parameters:
+	xOffset = 0;			// less = left, more = right
+	yOffset = 0;			// less = down, more = up
+	frequency = 0.105;		// less = less diversity/zoom in, more = more diversity/zoom out
+	fbmEnabled = false;		// "Fractional Brownian Motion"
+	numOctaves = 4;			// less = simpler, more = more complicated
+
+	// Control Parameters:
+	largeOffsetChange = 10;
+	smallOffsetChange = 1;
+	largeFrequencyChange = 0.1;
+	smallFrequencyChange = 0.01;
+	largeTextureWidthChange = 10;
+	smallTextureWidthChange = 1;
+
 	// Methods:
 	constructor() {
 		super("proceduralMapGenerationScene");
@@ -22,7 +31,7 @@ class ProceduralMapGeneration extends Phaser.Scene
 		this.initializeTexture();
 		this.initializeControls();
 
-		// Set seed
+		// Set noise seed
 		noise.seed(Math.random());
 
 		// Generate initial texture
@@ -37,183 +46,115 @@ class ProceduralMapGeneration extends Phaser.Scene
 	initializeTexture()
 	{
 		// Initialize variables
-		// notice that these variables are attributes of this scene class
-		// we do this because we want them to persist past this function call and also have other functions be able to access them
-		// i didn't put them along with the parameter variables at the top of this script because i only wanted variables whose values i'd be changing up there
 		this.textureWidth = game.config.width;
 		this.texture = [];
 
-		// Fill in texture with red squares (pixels)
-		// the squares' colors will be changed later when we generate the texture
-		// we set the squares to red initially because then they'll show if something went wrong with the texture generation
+		// Fill in texture with red squares (pixels) as placeholders
 		for (let y = 0; y < this.textureWidth; y++) {
 			this.texture[y] = [];
 			for (let x = 0; x < this.textureWidth; x++) {
-				this.texture[y][x] = this.add.rectangle(x, y, 1, 1, 0xff0000).setOrigin(0, 0);		// 0xff0000 = red
+				this.texture[y][x] = this.add.rectangle(x, y, 1, 1, 0xff0000).setOrigin(0);		// 0xff0000 = red
 			}
 		}
-	}
-
-	generateTexture()
-	{
-		if (!this.fbmEnabled) {
-			// Use the perlin noise function to get the color value for each square in texture
-
-			for (let y = 0; y < this.textureWidth; y++) {
-				for (let x = 0; x < this.textureWidth; x++) {
-
-					// Get a perlin value thats between [-1, 1]
-					let value = noise.perlin2((x + this.xOffset) * this.frequency, (y - this.yOffset) * this.frequency);
-
-					// Transform the value to be between [0, 1]
-					value = (value + 1) / 2;
-					//value = Math.floor(Math.abs(value) * 255);		// different way of changing the range to [0, 1] that produces a different looking type of texture
-
-					// Transform the value to be a whole number between [0, 255]
-					value = Math.floor(value * 255);
-
-					// Change the square's color
-					this.texture[y][x].fillColor = Phaser.Display.Color.GetColor(value, value, value);
-					
-				}
-			}
-		}
-		else {
-			// Use the perlin noise function to get the color value for each square in texture
-
-			for (let y = 0; y < this.textureWidth; y++) {
-				for (let x = 0; x < this.textureWidth; x++) {
-					
-					let result = 0;
-					let amplitude = 1;
-					let frequency = this.frequency;
-					for (let octave = 0; octave < this.numOctaves; octave++) {
-						const n = amplitude * noise.perlin2((x + this.xOffset) * frequency, (y - this.yOffset) * frequency);
-						result += n;
-						amplitude *= 0.5;
-						frequency *= 2;
-					}
-
-					// Transform the value to be between [0, 1]
-					result = (result + 1) / 2;
-					//value = Math.floor(Math.abs(value) * 255);		// different way of changing the range to [0, 1] that produces a different looking type of texture
-
-					// Transform the value to be a whole number between [0, 255]
-					result = Math.floor(result * 255);
-
-					// Change the square's color
-					this.texture[y][x].fillColor = Phaser.Display.Color.GetColor(result, result, result);
-					
-				}
-			}
-		}
-		
-		// Get debug info on the texture
-		this.debug();
 	}
 
 	initializeControls()
 	{
-		// Parameters:
-		const largeOffsetChange = 10;
-		const smallOffsetChange = 1;
-		const largeFrequencyChange = 0.1;
-		const smallFrequencyChange = 0.01;
-		const largeTextureWidthChange = 10;
-		const smallTextureWidthChange = 1;
-
-		// Variables:
+		// Initialize variables
 		this.startingXOffset = this.xOffset;
 		this.startingYOffset = this.yOffset;
 		this.startingFrequency = this.frequency;
+		this.startingFBMEnabled = this.fbmEnabled;
+		this.startingNumOctaves = this.numOctaves;
+
+		// Initialize input keys
 		this.moveUpKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
 		this.moveDownKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
 		this.moveLeftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
 		this.moveRightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-		this.increaseFrequencyKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
-		this.decreaseFrequencyKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+		this.increaseFrequencyKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+		this.decreaseFrequencyKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
 		this.shiftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
-		this.changeSeedKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-		this.resetKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+		this.toggleFBMKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+		this.increaseOctavesKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+		this.decreaseOctavesKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+		this.changeSeedKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
+		this.resetKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
 
-		// Events:
+		// Display controls
+		const controls = `
+		Move: WASD | SHIFT + WASD <br>
+		Change Freqency: LEFT/RIGHT | SHIFT + LEFT/RIGHT <br>
+		<br>
+		FBM Toggle: F <br>
+		Change Octaves: UP/DOWN <br>
+		<br>
+		Change Seed: C <br>
+		Reset: R
+		`;
+		document.getElementById("description").innerHTML = controls;
+
+		// Create events
 		this.moveUpKey.on("down", (key, event) => {						// move up
 			if (this.shiftKey.isDown) {
-				this.yOffset += largeOffsetChange;
+				this.yOffset += this.largeOffsetChange;
 			}
 			else {
-				this.yOffset += smallOffsetChange;
+				this.yOffset += this.smallOffsetChange;
 			}
 			this.generateTexture();
 			console.log(`moved to (${this.xOffset}, ${this.yOffset})`)
 		});
 		this.moveDownKey.on("down", (key, event) => {					// move down
 			if (this.shiftKey.isDown) {
-				this.yOffset -= largeOffsetChange;
+				this.yOffset -= this.largeOffsetChange;
 			}
 			else {
-				this.yOffset -= smallOffsetChange;
+				this.yOffset -= this.smallOffsetChange;
 			}
 			this.generateTexture();
 			console.log(`moved to (${this.xOffset}, ${this.yOffset})`)
 		});
 		this.moveLeftKey.on("down", (key, event) => {					// move left
 			if (this.shiftKey.isDown) {
-				this.xOffset -= largeOffsetChange;
+				this.xOffset -= this.largeOffsetChange;
 			}
 			else {
-				this.xOffset -= smallOffsetChange;
+				this.xOffset -= this.smallOffsetChange;
 			}
 			this.generateTexture();
 			console.log(`moved to (${this.xOffset}, ${this.yOffset})`)
 		});
 		this.moveRightKey.on("down", (key, event) => {					// move right
 			if (this.shiftKey.isDown) {
-				this.xOffset += largeOffsetChange;
+				this.xOffset += this.largeOffsetChange;
 			}
 			else {
-				this.xOffset += smallOffsetChange;
+				this.xOffset += this.smallOffsetChange;
 			}
 			this.generateTexture();
 			console.log(`moved to (${this.xOffset}, ${this.yOffset})`)
 		});
 		this.increaseFrequencyKey.on("down", (key, event) => {			// increase frequency
 			if (this.shiftKey.isDown) {
-				this.frequency += largeFrequencyChange;
+				this.frequency += this.largeFrequencyChange;
 			}
 			else {
-				this.frequency += smallFrequencyChange;
+				this.frequency += this.smallFrequencyChange;
 			}
 			this.generateTexture();
 			console.log(`frequency = ${this.frequency}`)
 		});
 		this.decreaseFrequencyKey.on("down", (key, event) => {			// decrease frequency
 			if (this.shiftKey.isDown) {
-				this.frequency -= largeFrequencyChange;
+				this.frequency -= this.largeFrequencyChange;
 			}
 			else {
-				this.frequency -= smallFrequencyChange;
+				this.frequency -= this.smallFrequencyChange;
 			}
 			this.generateTexture();
 			console.log(`frequency = ${this.frequency}`)
 		});
-		this.changeSeedKey.on("down", (key, event) => {					// change seed
-			noise.seed(Math.random());
-			this.generateTexture();
-			console.log("changed seed");
-		});
-		this.resetKey.on("down", (key, event) => {						// reset
-			this.xOffset = this.startingXOffset;
-			this.yOffset = this.startingYOffset;
-			this.frequency = this.startingFrequency;
-			this.generateTexture();
-			console.log("reset");
-		});
-
-		this.fbmEnabled = false;
-		this.toggleFBMKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-		this.increaseOctavesKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-		this.decreaseOctavesKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
 		this.toggleFBMKey.on("down", (key, event) => {					// toggle FBM
 			this.fbmEnabled = !this.fbmEnabled;
 			this.generateTexture();
@@ -234,6 +175,67 @@ class ProceduralMapGeneration extends Phaser.Scene
 			this.generateTexture();
 			console.log(`octaves = ${this.numOctaves}`);
 		});
+		this.changeSeedKey.on("down", (key, event) => {					// change seed
+			noise.seed(Math.random());
+			this.generateTexture();
+			console.log("changed seed");
+		});
+		this.resetKey.on("down", (key, event) => {						// reset
+			this.xOffset = this.startingXOffset;
+			this.yOffset = this.startingYOffset;
+			this.frequency = this.startingFrequency;
+			this.fbmEnabled = this.startingFBMEnabled;
+			this.numOctaves = this.startingNumOctaves;
+			this.generateTexture();
+			console.log("reset");
+		});
+	}
+
+	generateTexture()
+	{
+		// Use the perlin noise function to get the color value for each square in texture
+		for (let y = 0; y < this.textureWidth; y++) {
+			for (let x = 0; x < this.textureWidth; x++) {
+
+				let result = 0;
+				if (this.fbmEnabled) {		// FBM enabled
+					
+					// Stack octaves of the same position to get a perlin value thats usually between [-1, 1]
+					let amplitude = 1;
+					let frequency = this.frequency;
+					for (let octave = 0; octave < this.numOctaves; octave++) {
+						const octaveResult = amplitude * noise.perlin2((x + this.xOffset) * frequency, (y - this.yOffset) * frequency);
+						result += octaveResult;
+						amplitude *= 0.5;
+						frequency *= 2;
+					}
+
+					// Clamp result so it's between [-1, 1]
+					result = Phaser.Math.Clamp(result, -1, 1);
+
+				}
+				else {						// FBM disabled
+
+					// Get a perlin value thats between [-1, 1]
+					result = noise.perlin2((x + this.xOffset) * this.frequency, (y - this.yOffset) * this.frequency);
+
+				}
+				
+				// Transform the value to be between [0, 1]
+				result = (result + 1) / 2;
+				//value = Math.floor(Math.abs(value) * 255);		// different way of changing the range to [0, 1] that produces a different looking type of texture
+
+				// Transform the value to be a whole number between [0, 255]
+				result = Math.floor(result * 255);
+
+				// Change the square's color
+				this.texture[y][x].fillColor = Phaser.Display.Color.GetColor(result, result, result);
+
+			}
+		}
+		
+		// Get debug info on the texture
+		this.debug();
 	}
 
 	debug()
@@ -259,12 +261,8 @@ class ProceduralMapGeneration extends Phaser.Scene
 		for (let y = 0; y < this.textureWidth; y++) {
 			for (let x = 0; x < this.textureWidth; x++) {
 				const value = textureColors[y][x];
-				if (value < min) {
-					min = value;
-				}
-				else if (value > max) {
-					max = value;
-				}
+				min = Math.min(min, value);
+				max = Math.max(max, value);
 				total += value;
 			}
 		}
